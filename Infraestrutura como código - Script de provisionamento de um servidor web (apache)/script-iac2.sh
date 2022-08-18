e62d14f58d9a8aa3306ca0c3d8b2bf02fa67cf26
Pedro <104221500+pmd1308@users.noreply.github.com>
2022-08-18 08:01:42 -0300

    Update script-iac2.sh

--- Infraestrutura como código - Script de provisionamento de um servidor web (apache)/script-iac2.sh
+++ Infraestrutura como código - Script de provisionamento de um servidor web (apache)/script-iac2.sh
@@ -2,12 +2,9 @@
 echo "Atualizando"
 apt-get update
 apt-get upgrade -y
-
 echo "Instalando os componentes"
 apt-get install apache2 unzip -y
-
 echo "Baixando e copiando arquivos da aplicação"
-
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip

01532a82f7bbf3b2f0afffd59a6faa9b6becf7d1
Pedro <104221500+pmd1308@users.noreply.github.com>
2022-08-18 08:01:15 -0300

    up

---
+++ Infraestrutura como código - Script de provisionamento de um servidor web (apache)/script-iac2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Atualizando"
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando os componentes"
+apt-get install apache2 unzip -y
+
+echo "Baixando e copiando arquivos da aplicação"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
